dc75a4bad60d887d898f585befcf68562b640caf
Julie Chan <juliechan03@gmail.com>
2023-02-14 14:16:59 -0800

    first commit

--- flask_app/controllers/user.py
+++ flask_app/controllers/user.py
@@ -2,12 +2,39 @@ from flask import Flask, render_template, request, redirect
 from flask_app.models.users import User
 from flask_app import app
 
-@app.route('/display_all', methods=['GET'])
+# ALL USERS/READ
+@app.route('/', methods=['GET'])
 def display():
-    users = User.get_all()
-    print(users)
-    return render_template("read.html", users=users)
+    all_users = User.get_all()
+    print(all_users)
+    return render_template("read.html", users=all_users)
 
+# READ ONE
+@app.route('/user/show/<int:id>', methods=['GET'])
+def one(id):
+    user = User.get_one(id)
+    print(user)
+    return render_template("one_user.html", user=user)
+
+# UPDATE
+@app.route('/edit/<int:id>', methods=['GET'])
+def update(id):
+    user = User.get_one(id)
+    return render_template("edit_user.html", user=user)
+
+@app.route('/edit_user/<int:id>', methods=['POST']) #work on this
+def edit(id):
+    data = {"id":id, "first_name":request.form['first_name'], "last_name":request.form['last_name'], "email":request.form['email']}
+    User.update(data)
+    return redirect('/')
+
+# DELETE
+@app.route('/remove_user/<int:id>')
+def delete(id):
+    User.delete(id)
+    return redirect('/')
+
+# CREATE
 @app.route('/new_user', methods=['GET'])
 def form():
     return render_template("create.html")
@@ -15,4 +42,4 @@ def form():
 @app.route('/create_user', methods=['POST'])
 def user():
     User.save(request.form)
-    return redirect('/display_all')
+    return redirect('/')
--- flask_app/models/users.py
+++ flask_app/models/users.py
@@ -7,15 +7,44 @@ class User:
         self.first_name = data['first_name']
         self.last_name = data['last_name']
         self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
 
-    @classmethod
-    def save(cls,data):
+    @classmethod # SAVE/CREATE
+    def save(cls, data):
         query = """INSERT INTO users (first_name,last_name,email)
                 VALUES (%(first_name)s,%(last_name)s,%(email)s);"""
         result = connectToMySQL(cls.DB).query_db(query,data)
         return result
     
-    @classmethod
+    @classmethod # UPDATE
+    def update(cls, data):
+        query = """UPDATE users 
+                SET first_name = %(first_name)s, 
+                last_name = %(last_name)s, 
+                email = %(email)s
+                WHERE id = %(id)s;"""
+        results = connectToMySQL(cls.DB).query_db(query,data)
+        return results
+    
+    @classmethod # DELETE
+    def delete(cls, id):
+        query = """
+                DELETE FROM users
+                WHERE id = %(id)s;
+        """
+        data = {'id':id}
+        results = connectToMySQL(cls.DB).query_db(query,data)
+        return results
+    
+    @classmethod # READ ONE
+    def get_one(cls, id):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        data = {'id':id}
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(results[0])
+
+    @classmethod # ALL USERS/READ
     def get_all(cls):
         query = "SELECT * FROM users;"
         results = connectToMySQL(cls.DB).query_db(query)
